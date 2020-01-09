9715b1f29e22e62bcaf50d0f560847b3a535970d
krirati <kriratik@kkumail.com>
2020-01-09 12:08:46 +0700

    class

--- Jan8/classification.js
+++ Jan8/classification.js
@@ -1,31 +1,37 @@
 const tf = require('@tensorflow/tfjs');
 
+let t12, t15, t30, t33
 async function readData() {
     data = []
-    const csvDataset = tf.data.csv(
-        'file://./BLEdata.csv', {
-        columnConfigs: {
-            medv: {
-                isLabel: true
-            }
-        }
-    });
-    // const dataset = tf.data.csv('file://./BLEdata.csv', { hasHeader: true });
+    const rssi = [];
+    const labelR = [];
+    const numLine = 0
+    class_names = ['12', '15', '30', '33']
+    // const csvDataset = tf.data.csv(
+    //     'file://./BLEdata.csv', {
+    //     columnConfigs: {
+    //         medv: {
+    //             isLabel: true
+    //         }
+    //     }
+    // });
+    const dataset = tf.data.csv('file://./BLEdata.csv', { hasHeader: true });
+
     // const v = await dataset.take(2).toArray();
     // v.forEach((line) => {
-    //     data.push(line);
+    //     numLine++
+    //     rssi.push(line);
+    //     labelR.push() //จะเอาlabel ผลสุดท้าย
     // });
-    const numOfFeatures = (await csvDataset.columnNames()).length - 1;
-    const flattenedDataset =
-        csvDataset
-            .map(({ xs, ys }) => {
-                // Convert xs(features) and ys(labels) from object form (keyed by column
-                // name) to array form.
-                return { xs: Object.values(xs), ys: Object.values(ys) };
-            })
-            .batch(10);
+    for (let record of dataset) {
+        let data_rssi = [record.device , record.board12, record.board15 ,record.board30, record.board33];
+        rssi.push(data_rssi);
+        labelR.push(record.near)
+    }
 
-    return flattenedDataset , numOfFeatures;
+    const xs = tf.tensor2d(rssi);//ไม่แน่ใจ
+    const ys = tf.tensor1d(labelR, 'int32 ')//ไม่แน่ใจ
+    return { xs, ys };
 }
 function createModel() {
 
@@ -33,7 +39,7 @@ function createModel() {
     let hidden = tf.layers.dense({
         units: 16,
         activation: 'sigmoid',
-        inputDim: [2]
+        inputShape: 6
     });
     let output = tf.layers.dense({
         units: 9,
@@ -53,21 +59,45 @@ function createModel() {
 }
 
 
-async function trainModel(model, flattenedDataset, epochs) {
+async function trainModel(model, xs, ys) {
+    // const options = {
+    //     epochs: 30,
+    //     validationData: 0.1,
+    //     shuffle: true,
+    //     callbacks: {
+    //         onTrainBegin: () => console.log('train start'),
+    //         onTrainEnd: () => console.log('train complete'),
+    //         onBatchEnd: tf.nextFrame,
+    //         onEpochEnd: (num, logs) => {
+    //             console.log('Epoch: ' + num);
+
+    //         }
+    //     }
+    // }
+    // await model.fit(xs, ys, options).then(results => {
+    //     console.log(results.history.loss);
+    // })
     const loss_arr = [];
-    await model.fitDataset(flattenedDataset, {
-        epochs: epochs,
-        validationSplit: 0.1,
+    await model.fit(xs, ys, {
+        epochs: 30,
+        validationData: 0.1,
+        shuffle: true,
         callbacks: {
+            onTrainBegin: () => console.log('train start'),
+            onTrainEnd: () => console.log('train complete'),
+            onBatchEnd: tf.nextFrame,
             onEpochEnd: (epoch, log) => loss_arr.push(log.loss)
         }
     });
-
     return loss_arr;
 }
 
-function predictModel(model, flattenedDataset) {
-    const tf_xv = tf.tensor1d(flattenedDataset);
-    const yv = model.predict(tf_xv).dataSync();
-    return yv;
+function predictModel(model,xs) {
+    const tf_xv = tf.tensor2d(xs); //ไม่แน่ใจ 
+    const yv = model.predict(tf_xv);
+    let index = yv.argMax(1).dataSync()[0];
+    console.log(index)
+    let label = class_names[index]
+    console.log(label);
+    return yv, label;
 }
